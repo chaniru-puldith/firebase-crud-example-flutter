b6ee017d340a8ea27ac43543e8e4ef6bb9276450
chaniru-puldith <a.a.d.chaniru.p@gmail.com>
2023-10-06 14:24:17 +0530

    Reformat Code

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:firebase_crud_example/screens/add_product_screen.dart';
-import 'package:firebase_crud_example/screens/home_screen.dart';
-import 'package:firebase_crud_example/screens/login_screen.dart';
-import 'package:firebase_crud_example/screens/register_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -21,7 +18,7 @@ class MyApp extends StatelessWidget {
         systemNavigationBarColor: Colors.transparent,
       ),
     );
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: AddProductScreen(),
     );
